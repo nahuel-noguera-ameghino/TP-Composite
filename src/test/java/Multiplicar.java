7a75f432cec44f40a2e0bff3540f0bee7f323567
victoria-heredia-ameghino <victoria-29-10-12@hotmail.com>
2016-09-29 15:53:49 -0300

    Clase del 29/9

--- src/test/java/Multiplicar.java
+++ src/test/java/Multiplicar.java
@@ -1,8 +1,12 @@
 /**
  * Created by alumno on 29/09/16.
  */
-public class Multiplicar {
+public class Multiplicar implements Expresiones {
 
+    Expresiones expresion1;
+    Expresiones expresion2;
 
-    public Multiplicar()
+    public double evaluar() {
+        return (this.expresion1.evaluar() * this.expresion2.evaluar());
+    }
 }
